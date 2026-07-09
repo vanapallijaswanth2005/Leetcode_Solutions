1bb834e6d0dda1ba92b5e4e8b094ff86a5768789
Jaswanth Vanapalli <vanapallijaswanth2005@gmail.com>
2026-07-09 19:42:26 +0530

    Time: 0 ms (100%), Space: 42.2 MB (85.06%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        if(arrivalTime+delayedTime<24)
+        {
+            return arrivalTime+delayedTime;
+
+        }
+        else if(arrivalTime+delayedTime>24)
+        {
+            return (arrivalTime+delayedTime)-24;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
